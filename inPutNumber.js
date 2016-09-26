0019255ced8651eddb794358f2fa0c059586b2c0
Andy Lin <lieyingllh@163.com>
2016-09-26 18:35:12 +0800

    Update inPutNumber.js

--- inPutNumber.js
+++ inPutNumber.js
@@ -1,6 +1,4 @@
-<script>
 var firstNumber = prompt("Enter the first number");
 var secondNumber = prompt("Enter the second number");
 var total = Number(firstNumber) + Number(secondNumber);
 alert (total);
-</script>
