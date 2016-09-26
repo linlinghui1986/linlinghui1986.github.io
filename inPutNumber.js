90306349f479b369451425c304f20cb868d86f80
Andy Lin <lieyingllh@163.com>
2016-09-26 18:30:35 +0800

    Create inPutNumber.js

---
+++ inPutNumber.js
@@ -0,0 +1,6 @@
+<script>
+var firstNumber = prompt("Enter the first number");
+var secondNumber = prompt("Enter the second number");
+var total = Number(firstNumber) + Number(secondNumber);
+alert (total);
+</script>
