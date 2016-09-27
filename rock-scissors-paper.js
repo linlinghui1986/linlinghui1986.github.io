eee37ce57d3b507dd7f278aa24baf6daa52af680
Andy Lin <lieyingllh@163.com>
2016-09-27 14:57:20 +0800

    Update rock-scissors-paper.js

--- rock-scissors-paper.js
+++ rock-scissors-paper.js
@@ -1,18 +1,16 @@
 var compare=function(choice1,choice2){
 if(choice1===choice2){return("和林凌辉打成平手");}
 else{
-	if(choice1==="石头"&choice2==="剪子"){ console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
+	if(choice1==="石头"&choice2==="剪子"){ console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
     else{
-		if(choice1==="石头"&choice2==="布"){console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你输了！");}
+		if(choice1==="石头"&choice2==="布"){console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你输了！");}
 		else{
-			if(choice1==="剪子"&choice2==="石头"){console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你输了！");}
+			if(choice1==="剪子"&choice2==="石头"){console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你输了！");}
 			else{
-				if(choice1==="剪子"&choice2==="布"){console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
+				if(choice1==="剪子"&choice2==="布"){console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
  				else{
-					if(choice1==="布"&choice2==="石头"){console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
+					if(choice1==="布"&choice2==="石头"){console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你赢了！");}
 					else{
-						if(choice1==="布"&choice2==="剪子"){console.log("你出"+useChoice+"，林凌辉出"+computerChoice+"，你输了！");}
+						if(choice1==="布"&choice2==="剪子"){console.log("你出"+userChoice+"，林凌辉出"+computerChoice+"，你输了！");}
 						else{console.log("游戏结束");}
 }}}}}}}
-    
-compare(userChoice,computerChoice)
